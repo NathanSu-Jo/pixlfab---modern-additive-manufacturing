1eae75bfc6ee3ba76170fde8f14f6b399c0f589c
Joel Su <ianpheysu@gmail.com>
2026-01-05 22:30:31 +0800

    Update: replace placeholder images with local images and add logo

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -45,7 +45,7 @@ const Hero: React.FC = () => {
           <div className="mt-16 lg:mt-0 relative">
             <div className="relative rounded-2xl overflow-hidden shadow-2xl bg-slate-200 aspect-[4/3]">
               <img 
-                src="https://picsum.photos/seed/industrial/800/600" 
+                src="/pics/bgpic1.png" 
                 alt="3D Printing Production" 
                 className="w-full h-full object-cover mix-blend-multiply opacity-80"
               />
