bfe7ddacc20d720eeeb42a175573eeb91e6861f0
Joel Su <ianpheysu@gmail.com>
2026-01-05 21:25:59 +0800

    deploy to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/pf2',
+      base: '/pixlfab---modern-additive-manufacturing',
       server: {
         port: 3000,
         host: '0.0.0.0',
